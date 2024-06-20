db13309fd3f09d0e71fe157f70fc614a9b93eee7
zbcoding <zbcoding>
2024-06-20 13:41:47 -0500

    Add username to output

---
+++ connect.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+//Eris is a package for interacting with Discord
+const Eris = require("eris");
+
+const account = new Eris(process.env["TOKEN"]);
+
+// Export the username
+module.exports.getUsername = () => account.username;
+
+// Export the connect function
+module.exports.connect = () => {
+  account.on("error", async (err) => {
+    let message = "";
+    if (err.toString().includes("Error: Invalid token")) {
+      message = `An error occurred in the Discord bot. 
+                  Probably invalid token. 
+                  See replit.\n`;
+
+      const webhookURL = process.env["WEBHOOK_URL"];
+
+      const fetch = await import("node-fetch");
+
+      await fetch.default(webhookURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: message }),
+      });
+      console.log("Sent error notification to Discord.");
+    } else if (
+      /*TODO refactor */
+      /*common errors you can choose to ignore*/
+      err
+        .toString()
+        .includes(
+          "TypeError: Cannot read properties of undefined (reading 'add')"
+        )
+    ) {
+      console.log(`An error occurred in the Discord bot. ${err}`);
+    } else if (
+      err
+        .toString()
+        .includes(
+          "TypeError: Cannot read properties of undefined (reading 'get')"
+        )
+    ) {
+      console.log(`An error occurred in the Discord bot. ${err}`);
+    } else if (err.toString().includes("Error: Connection reset by peer")) {
+      console.log(`An error occurred in the Discord bot. ${err}`);
+    } else {
+      /*other unknown errors can be sent to low importance discord channel*/
+      message = `An error occurred in the Discord bot. ${err}\n`;
+
+      const webhookURL = process.env["WEBHOOK_URL_LOW"];
+
+      const fetch = await import("node-fetch");
+
+      await fetch.default(webhookURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: message }),
+      });
+
+      console.log("Sent error notification to Discord.");
+    }
+  });
+  account.connect();
+};
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const express = require("express");
 const server = express();
-const { username } = require('./index');
+const { getUsername } = require('./connect');
 
 server.get('/favicon.ico', (req, res) => res.status(204));
 server.all("/", (req, res) => {
   const date = Date();
   console.log("Just got a request!");
+  const username = getUsername();
   const firstUserNameChar = username ? `${username[0]}` : '?';
   res.send(`Your account ${firstUserNameChar} is alive! ${date}`);
 });
 
 function keepAlive() {
   const port = process.env.PORT || 3000;
-
   server.listen(port, '0.0.0.0', (err) => {
     if (err) {
       console.error('Error starting server:', err);
@@ -22,7 +22,6 @@ function keepAlive() {
       console.log("Server is Ready!", date);
     }
   });
-
   server.on('error', (err) => {
     console.error('Server error:', err);
   });
